08c8d9341334adf3a2854a6f2f4ed2274799ec6b
cmleem <coals722@naver.com>
2024-05-24 17:58:26 +0900

    first commit

---
+++ src/main/java/com/charmroom/charmroom/CharmroomApplication.java
@@ -0,0 +1,13 @@
+package com.charmroom.charmroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharmroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharmroomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/charmroom/charmroom/CharmroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charmroom.charmroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharmroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
