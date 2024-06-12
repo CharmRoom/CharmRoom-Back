82362861dde02b383e63f5d271543d5ca3a407e1
Yeop-Dong <dong3855@gmail.com>
2024-06-12 22:13:53 +0900

    fix: DEV CICD Settings

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+CURRENT_PID=$(pgrep -f .jar)
+if [ -z $CURRENT_PID ]; then
+	echo "Server is not running now"
+else
+	echo "kill Server"
+	kill -9 $CURRENT_PID
+	sleep 3
+fi
+
+JAR_PATH="~/cicd/*.jar"
+chmod +x $JAR_PATH
+nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+echo "Deploy Successfully Finished!!!"
