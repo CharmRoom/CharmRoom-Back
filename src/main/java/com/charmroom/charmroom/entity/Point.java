7fbf55a60abc026971f04883fb5181af946c26b4
Yeop-Dong <dong3855@gmail.com>
2024-06-12 19:11:17 +0900

    feat: Point Entity
    포인트 엔티티 작성
    PointType Enum 추가
    
    추후에 PointType의 종류에 대해서 의논 필요
    
    참고: #11

---
+++ src/main/java/com/charmroom/charmroom/entity/Point.java
@@ -0,0 +1,41 @@
+package com.charmroom.charmroom.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.charmroom.charmroom.entity.enums.PointType;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access=AccessLevel.PROTECTED)
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+public class Point {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@OneToOne
+	private User user;
+	
+	@CreationTimestamp
+	private LocalDateTime updatedAt;
+	
+	@Enumerated(EnumType.STRING)
+	private PointType type;
+}
