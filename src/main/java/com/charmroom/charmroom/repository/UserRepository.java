17bf604de4ec9a2666ded91e60d6513a7fb96528
cmleem <coals722@naver.com>
2024-06-13 18:17:14 +0900

    feat : User Repository 기능 추가
    User Repositpory에 닉네임을 기준으로 id를 조히하는 메서드 추가
    
    참고: #19

--- src/main/java/com/charmroom/charmroom/repository/UserRepository.java
+++ src/main/java/com/charmroom/charmroom/repository/UserRepository.java
@@ -4,5 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.charmroom.charmroom.entity.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByNickname(String nickname);
+
+
 }
