f2cb91228758d6b75f63445ef90c79b8fbf615d1
Yeop-Dong <dong3855@gmail.com>
2024-06-20 13:59:46 +0900

    fix: 비즈니스 로직 에러 구획 나누는 주석 추가
    참고: #35

--- src/main/java/com/charmroom/charmroom/exception/BusinessLogicError.java
+++ src/main/java/com/charmroom/charmroom/exception/BusinessLogicError.java
@@ -8,12 +8,18 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum BusinessLogicError {
-	UNKNOWN(HttpStatus.BAD_REQUEST, "00000", "Unknown"), 
+	
+	// Unknown
+	UNKNOWN(HttpStatus.BAD_REQUEST, "00000", "Unknown"),
+	// end Unknown
+	
+	// User
 	DUPLICATED_USERNAME(HttpStatus.BAD_REQUEST, "12000", "Duplicated username"),
 	DUPLICATED_EMAIL(HttpStatus.BAD_REQUEST, "12001", "Duplicated email"),
 	DUPLICATED_NICKNAME(HttpStatus.BAD_REQUEST, "12002", "Duplicated nickname"),
 	
 	NOTFOUND_USER(HttpStatus.NOT_FOUND, "12100", "User not found")
+	// end User
 	;
 	private final HttpStatus status;
 	private final String code;
