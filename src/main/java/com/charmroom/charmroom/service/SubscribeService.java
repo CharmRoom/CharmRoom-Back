04eee87d5684c917354cb4a971f0d65887187da2
cmleem <coals722@naver.com>
2024-06-23 04:17:23 +0900

    feat : Subscribe Service
    SubscribeService, SubscribeServiceUnitTest 작성
    
    참고: #36

---
+++ src/main/java/com/charmroom/charmroom/service/SubscribeService.java
@@ -0,0 +1,41 @@
+package com.charmroom.charmroom.service;
+
+import com.charmroom.charmroom.entity.Subscribe;
+import com.charmroom.charmroom.entity.User;
+import com.charmroom.charmroom.exception.BusinessLogicError;
+import com.charmroom.charmroom.exception.BusinessLogicException;
+import com.charmroom.charmroom.repository.SubscribeRepository;
+import com.charmroom.charmroom.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class SubscribeService {
+    private final SubscribeRepository subscribeRepository;
+    private final UserRepository userRepository;
+
+    public Subscribe create(String subscriberName, String targetName) {
+        User subscriber = userRepository.findByUsername(subscriberName).orElseThrow(() ->
+                new BusinessLogicException(BusinessLogicError.NOTFOUND_USER, "subscriberName: " + subscriberName));
+        User target = userRepository.findByUsername(targetName).orElseThrow(() ->
+                new BusinessLogicException(BusinessLogicError.NOTFOUND_USER, "targetName: " + targetName));
+
+        Optional<Subscribe> found = subscribeRepository.findBySubscriberAndTarget(subscriber, target);
+
+        if (found.isPresent()) {
+            Subscribe subscribe = found.get();
+            subscribeRepository.delete(subscribe);
+            return null; // 구독 취소
+        } else {
+            return subscribeRepository.save(
+                    Subscribe.builder()
+                            .subscriber(subscriber)
+                            .target(target)
+                            .build()
+            );
+        }
+    }
+}
