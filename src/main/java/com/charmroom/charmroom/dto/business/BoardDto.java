877b58f4222ee1f41d33a9fd90d5287ff3e25966
Yeop-Dong <dong3855@gmail.com>
2024-06-27 00:08:03 +0900

    feat: Board service DTO
    참고: #60, #68

---
+++ src/main/java/com/charmroom/charmroom/dto/business/BoardDto.java
@@ -0,0 +1,17 @@
+package com.charmroom.charmroom.dto.business;
+
+import com.charmroom.charmroom.entity.enums.BoardType;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class BoardDto {
+	private Integer id;
+	private String name;
+	private BoardType type;
+	private Boolean exposed;
+}
