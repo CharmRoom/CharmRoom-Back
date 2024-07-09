8e92a95e0cfa3acd68bb6ebc9342964054073911
cmleem <coals722@naver.com>
2024-07-09 16:16:24 +0900

    feat : WishService getWishesByUserName 메서드 추가
    참고 #84, #119

--- src/main/java/com/charmroom/charmroom/repository/WishRepository.java
+++ src/main/java/com/charmroom/charmroom/repository/WishRepository.java
@@ -4,10 +4,14 @@ import com.charmroom.charmroom.entity.Market;
 import com.charmroom.charmroom.entity.User;
 import com.charmroom.charmroom.entity.Wish;
 import com.charmroom.charmroom.entity.embid.WishId;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface WishRepository extends JpaRepository<Wish, WishId> {
     Optional<Wish> findByUserAndMarket(User user, Market market);
+
+    Page<Wish> findAllByUser(User user, Pageable pageable);
 }
