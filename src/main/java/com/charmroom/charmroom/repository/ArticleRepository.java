352d671896ef5716c721bef30fff352838181967
cmleem <coals722@naver.com>
2024-07-09 16:00:47 +0900

    feat : ArticleService 메서드 추가
    getArticlesByUsername 메서드 추가
    
    참고 #84, #119

--- src/main/java/com/charmroom/charmroom/repository/ArticleRepository.java
+++ src/main/java/com/charmroom/charmroom/repository/ArticleRepository.java
@@ -1,5 +1,6 @@
 package com.charmroom.charmroom.repository;
 
+import com.charmroom.charmroom.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +10,6 @@ import com.charmroom.charmroom.entity.Board;
 
 public interface ArticleRepository extends JpaRepository<Article, Integer> {
 	Page<Article> findAllByBoard(Board board, Pageable pageable);
+
+    Page<Article> findAllByUser(User user, Pageable pageable);
 }
