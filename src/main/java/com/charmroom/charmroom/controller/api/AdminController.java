73aca9f006bcea551c6bedcc1399cfe0e07ca24e
cmleem <coals722@naver.com>
2024-07-09 20:00:41 +0900

    feat : AdminController ad 관련 메서드 작성
    광고 관련 메서드 추가
    
    참고 #84, #120

--- src/main/java/com/charmroom/charmroom/controller/api/AdminController.java
+++ src/main/java/com/charmroom/charmroom/controller/api/AdminController.java
@@ -1,11 +1,15 @@
 package com.charmroom.charmroom.controller.api;
 
+import com.charmroom.charmroom.dto.business.AdMapper;
+import com.charmroom.charmroom.service.AdService;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,10 +21,14 @@ import org.springframework.web.bind.annotation.RestController;
 import com.charmroom.charmroom.dto.business.BoardMapper;
 import com.charmroom.charmroom.dto.business.PointMapper;
 import com.charmroom.charmroom.dto.business.UserMapper;
+import com.charmroom.charmroom.dto.business.AdDto;
+import com.charmroom.charmroom.dto.presentation.AdDto.AdCreateRequestDto;
+import com.charmroom.charmroom.dto.presentation.AdDto.AdResponseDto;
 import com.charmroom.charmroom.dto.presentation.BoardDto.BoardCreateRequestDto;
 import com.charmroom.charmroom.dto.presentation.BoardDto.BoardUpdateRequestDto;
 import com.charmroom.charmroom.dto.presentation.CommonResponseDto;
 import com.charmroom.charmroom.dto.presentation.PointDto.PointCreateRequestDto;
+import com.charmroom.charmroom.dto.presentation.AdDto.AdUpdateRequestDto;
 import com.charmroom.charmroom.service.BoardService;
 import com.charmroom.charmroom.service.PointService;
 import com.charmroom.charmroom.service.UserService;
@@ -34,80 +42,119 @@ public class AdminController {
 	private final UserService userService;
 	private final BoardService boardService;
 	private final PointService pointService;
-	
+	private final AdService adService;
+
 	@GetMapping("/user")
 	public ResponseEntity<?> users(
-			@PageableDefault(size=10, sort="id", direction=Sort.Direction.DESC) Pageable pageable){
+			@PageableDefault(size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
 		var dtos = userService.getAllUsersByPageable(pageable);
 		var response = dtos.map(dto -> UserMapper.toResponse(dto));
 		return CommonResponseDto.ok(response).toResponseEntity();
 	}
-	
+
 	@PatchMapping("/user/grade")
 	public ResponseEntity<?> changeUserGrade(
 			@RequestParam("username") String username,
 			@RequestParam("grade") String grade
-			){
+	) {
 		var dto = userService.changeLevel(username, grade);
 		var response = UserMapper.toResponse(dto);
 		return CommonResponseDto.ok(response).toResponseEntity();
 	}
-	
+
 	@PatchMapping("/user/withdraw")
 	public ResponseEntity<?> changeUserWithdraw(
 			@RequestParam("username") String username,
 			@RequestParam(name = "withdraw", defaultValue = "true") Boolean withdraw
-			){
+	) {
 		var dto = userService.changeWithdraw(username, withdraw);
 		var response = UserMapper.toResponse(dto);
 		return CommonResponseDto.ok(response).toResponseEntity();
 	}
-	
+
 	@PostMapping("/point/{username}")
 	public ResponseEntity<?> givePoint(
 			@PathVariable("username") String username,
 			@RequestBody PointCreateRequestDto request
-			){
+	) {
 		var dto = pointService.create(username, request.getType(), request.getDiff());
 		var response = PointMapper.toResponse(dto);
 		return CommonResponseDto.ok(response).toResponseEntity();
-		
+
 	}
+
 	@PostMapping("/board")
 	public ResponseEntity<?> createBoard(
 			@RequestBody BoardCreateRequestDto request
-			){
+	) {
 		var dto = boardService.create(request.getName(), request.getType());
 		var response = BoardMapper.toResponse(dto);
 		return CommonResponseDto.created(response).toResponseEntity();
 	}
-	
+
 	@PostMapping("/board/{boardId}")
 	public ResponseEntity<?> updateBoard(
 			@PathVariable("boardId") Integer boardId,
 			@RequestBody BoardUpdateRequestDto request
-			){
+	) {
 		var dto = boardService.update(boardId, request.getName(), request.getType());
 		var response = BoardMapper.toResponse(dto);
 		return CommonResponseDto.ok(response).toResponseEntity();
 	}
-	
+
 	@PatchMapping("/board/{boardId}")
 	public ResponseEntity<?> exposeBoard(
 			@PathVariable("boardId") Integer boardId,
-			@RequestParam("exposed") Boolean exposed 
-			) {
+			@RequestParam("exposed") Boolean exposed
+	) {
 		var dto = boardService.changeExpose(boardId, exposed);
 		var response = BoardMapper.toResponse(dto);
 		return CommonResponseDto.ok(response).toResponseEntity();
 	}
-	
+
 	@DeleteMapping("/board/{boardId}")
 	public ResponseEntity<?> deleteBoard(
 			@PathVariable("boardId") Integer boardId
-			){
+	) {
 		boardService.delete(boardId);
 		return CommonResponseDto.ok().toResponseEntity();
 	}
-	
+
+	@PostMapping("/ad")
+	public ResponseEntity<?> createAd(
+			@ModelAttribute AdCreateRequestDto request
+	) {
+		AdDto adDto = adService.create(request.getTitle(), request.getLink(), request.getImage(), request.getStart(), request.getEnd());
+
+		AdResponseDto response = AdMapper.toResponse(adDto);
+		return CommonResponseDto.created(response).toResponseEntity();
+	}
+
+	@GetMapping("/ad")
+	public ResponseEntity<?> getAds(
+			@PageableDefault(size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable
+	) {
+		Page<AdDto> dtos = adService.getAllAdsByPageable(pageable);
+		Page<AdResponseDto> response = dtos.map(dto -> AdMapper.toResponse(dto));
+		return CommonResponseDto.ok(response).toResponseEntity();
+	}
+
+	@PostMapping("/ad/{adId}")
+	public ResponseEntity<?> updateAd(
+			@PathVariable("adId") Integer adId,
+			@ModelAttribute AdUpdateRequestDto requestDto
+	) {
+		AdDto adDto = adService.updateAd(adId, requestDto.getTitle(), requestDto.getLink(), requestDto.getStart(), requestDto.getEnd(), requestDto.getImage());
+
+		AdResponseDto response = AdMapper.toResponse(adDto);
+		return CommonResponseDto.ok(response).toResponseEntity();
+	}
+
+	@DeleteMapping("/ad/{adId}")
+	public ResponseEntity<?> deleteAd(
+			@PathVariable("adId") Integer adId
+	) {
+		adService.deleteAd(adId);
+		return CommonResponseDto.ok().toResponseEntity();
+	}
 }
---
+++ src/test/java/com/charmroom/charmroom/controller/unit/AdminControllerUnitTestCm.java
@@ -0,0 +1,196 @@
+package com.charmroom.charmroom.controller.unit;
+
+import com.charmroom.charmroom.controller.api.AdminController;
+import com.charmroom.charmroom.dto.business.AdDto;
+import com.charmroom.charmroom.exception.BusinessLogicError;
+import com.charmroom.charmroom.exception.BusinessLogicException;
+import com.charmroom.charmroom.exception.ExceptionHandlerAdvice;
+import com.charmroom.charmroom.service.AdService;
+import com.google.gson.Gson;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.validation.Validator;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Random;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+public class AdminControllerUnitTestCm {
+    @Mock
+    AdService adService;
+
+    @InjectMocks
+    AdminController adminController;
+
+    MockMvc mockMvc;
+    AdDto mockedAdDto;
+
+    Gson gson;
+
+    @BeforeEach
+    void setup() {
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(adminController)
+                .setControllerAdvice(new ExceptionHandlerAdvice())
+                .setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver())
+                .setValidator(mock(Validator.class))
+                .build();
+
+        mockedAdDto = AdDto.builder()
+                .id(1)
+                .title("title")
+                .start(LocalDateTime.now())
+                .end(LocalDateTime.now().plusDays(1))
+                .link("")
+                .build();
+
+        gson = new Gson();
+    }
+
+    @Nested
+    class CreateAdd {
+        @Test
+        void success() throws Exception {
+            // given
+            MockMultipartFile imageFile = new MockMultipartFile("image", "test.png", MediaType.IMAGE_PNG_VALUE, "test".getBytes());
+
+            doReturn(mockedAdDto).when(adService).create(mockedAdDto.getTitle(), mockedAdDto.getLink(), imageFile, mockedAdDto.getStart(), mockedAdDto.getEnd());
+
+            // when
+            ResultActions resultActions = mockMvc.perform(multipart("/api/admin/ad")
+                    .file(imageFile)
+                    .param("title", mockedAdDto.getTitle())
+                    .param("link", mockedAdDto.getLink())
+                    .param("start", mockedAdDto.getStart().toString())
+                    .param("end", mockedAdDto.getEnd().toString())
+            );
+
+            // then
+            resultActions.andExpectAll(
+                    status().isCreated(),
+                    jsonPath("$.code").value("CREATED"),
+                    jsonPath("$.data.title").value(mockedAdDto.getTitle())
+            );
+        }
+    }
+
+    @Nested
+    class GetArticles {
+        @Test
+        void success() throws Exception {
+            // given
+            List<AdDto> dtoList = List.of(mockedAdDto, mockedAdDto, mockedAdDto);
+
+            PageRequest pageRequest = PageRequest.of(0, 10, Sort.by("id").descending());
+            PageImpl<AdDto> dtoPage = new PageImpl<>(dtoList, pageRequest, 3);
+
+            doReturn(dtoPage).when(adService).getAllAdsByPageable(pageRequest);
+
+            // when
+            ResultActions resultActions = mockMvc.perform(get("/api/admin/ad"));
+
+            // then
+            resultActions.andExpectAll(
+                    status().isOk(),
+                    jsonPath("$.data.content.size()").value(3),
+                    jsonPath("$.data.content[0].title").value(mockedAdDto.getTitle())
+            );
+        }
+    }
+
+    @Nested
+    class Update {
+        @Test
+        void success() throws Exception {
+            // given
+            MockMultipartFile imageFile = new MockMultipartFile("image", "test.png", MediaType.IMAGE_PNG_VALUE, "test".getBytes());
+
+            doReturn(mockedAdDto).when(adService).updateAd(mockedAdDto.getId(), mockedAdDto.getTitle(), mockedAdDto.getLink(), mockedAdDto.getStart(), mockedAdDto.getEnd(), imageFile);
+
+            // when
+            ResultActions resultActions = mockMvc.perform(multipart("/api/admin/ad/1")
+                    .file(imageFile)
+                    .param("title", mockedAdDto.getTitle())
+                    .param("link", mockedAdDto.getLink())
+                    .param("start", mockedAdDto.getStart().toString())
+                    .param("end", mockedAdDto.getEnd().toString())
+            );
+
+            // then
+            resultActions.andExpectAll(
+                    status().isOk(),
+                    jsonPath("$.data.title").value(mockedAdDto.getTitle())
+            );
+        }
+
+        @Test
+        void fail_NotFoundAd() throws Exception {
+            // given
+            MockMultipartFile imageFile = new MockMultipartFile("image", "test.png", MediaType.IMAGE_PNG_VALUE, "test".getBytes());
+
+            doThrow(new BusinessLogicException(BusinessLogicError.NOTFOUND_AD))
+                    .when(adService)
+                    .updateAd(mockedAdDto.getId(), mockedAdDto.getTitle(), mockedAdDto.getLink(), mockedAdDto.getStart(), mockedAdDto.getEnd(), imageFile);
+
+            // when
+            ResultActions resultActions = mockMvc.perform(multipart("/api/admin/ad/1")
+                    .file(imageFile)
+                    .param("title", mockedAdDto.getTitle())
+                    .param("link", mockedAdDto.getLink())
+                    .param("start", mockedAdDto.getStart().toString())
+                    .param("end", mockedAdDto.getEnd().toString()));
+
+            // then
+            resultActions.andExpectAll(
+                    status().isNotFound(),
+                    jsonPath("code").value("00100")
+            );
+        }
+    }
+
+    @Nested
+    class DeleteAd {
+        @Test
+        void success() throws Exception {
+            // given
+            doNothing().when(adService).deleteAd(eq(1));
+
+            // when
+            ResultActions resultActions = mockMvc.perform(delete("/api/admin/ad/1"));
+
+            // then
+            resultActions.andExpectAll(
+                    status().isOk(),
+                    jsonPath("$.code").value("OK")
+            );
+        }
+    }
+}
